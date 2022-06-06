41bc05fa8ba3b35fbb08f58a27a8c9f8e805a816
Gilberto Mata <gilmata@gmail.com>
2022-06-06 02:00:49 -0500

    lunes one

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:gilbmata/gilbmata.github.io.git main
+# git push -f git@github.com:GilbMata/GilbMata.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:GilbMata/drpmexico.git main:gh-pages
